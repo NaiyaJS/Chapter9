83d16debf8e37599e539dc477f2d119667929a82
NaiyaJS <NaiyaJS04@gmail.com>
2023-09-15 01:28:37 -0400

    adding woman and mother classes

--- src/main/java/chapter9/InheritanceTester.java
+++ src/main/java/chapter9/InheritanceTester.java
@@ -1,11 +1,34 @@
 package chapter9;
 
 public class InheritanceTester {
-    public static void main(String[] args){
 
-       Person person = new Person();
-       Employee employee = new Employee();
-       employee.
+    public static void main(String[] args) {
+        Mother mom = new Mother();
+        mom.setName("Rosa");
+
+        System.out.println(mom.getName() + " is a " + mom.getGender());
+    }
+    public static void testSquare(String[] args) {
+        Rectangle rectangle = new Rectangle();
+        rectangle.print();
+
+        Square square = new Square();
+        square.print(" square");
 
     }
+
+        public static void testSquareOverride() {
+            Rectangle rectangle = new Rectangle();
+            rectangle.setLength(5.0);
+            rectangle.setWidth(4.3);
+            System.out.println(rectangle.calculatePerimeter());
+
+            Square square = new Square();
+            square.setLength(5.0);
+            square.setWidth(3.0);
+            System.out.println(square.calculatePerimeter());
+
+        }
+
+
 }
